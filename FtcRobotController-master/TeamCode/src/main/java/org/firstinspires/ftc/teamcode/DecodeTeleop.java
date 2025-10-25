6687ba367ef7f86e7649a338565c81fac273cdd0
NutellaJello <alexky.gan@gmail.com>
2025-10-25 16:14:21 -0400

    DecodeTeleop now intakes and outtakes

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleop.java
@@ -1,37 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.subsystems.DecodeDriveTrain;
 
 
 @TeleOp(name = "DecodeTeleop", group = "TeleOp")
-
+@Disabled
 public class DecodeTeleop extends LinearOpMode{
     private DecodeDriveTrain drivetrain;
     private DcMotorEx intake;
+    private DcMotorEx outtake;
     boolean fieldCentric = true;
+    double intakePower = 0;
+    double outtakePower = 0;
 
 
 
     @Override
     public void runOpMode() {
         // initializes movement motors
-       // drivetrain = new DecodeDriveTrain(hardwareMap);
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        imu.initialize(parameters);
+        drivetrain = new DecodeDriveTrain(hardwareMap);
 
         intake=hardwareMap.get(DcMotorEx.class, "intake");
-        intake.setDirection(DcMotorEx.Direction.FORWARD);
+        intake.setDirection(DcMotorEx.Direction.REVERSE); // Change this to either FORWARD or REVERSE
+
+        outtake=hardwareMap.get(DcMotorEx.class, "outtake");
+        outtake.setDirection(DcMotorEx.Direction.FORWARD); // Change this to either FORWARD or REVERSE
+
 
         waitForStart();
         while (opModeIsActive()) {
@@ -40,16 +45,29 @@ public class DecodeTeleop extends LinearOpMode{
                 fieldCentric = !fieldCentric;
             }
             // all the movement controls.
-            //drivetrain.Teleop(gamepad1,telemetry, fieldCentric);
+            drivetrain.Teleop(gamepad1,telemetry, fieldCentric);
 
 
-            // delete the other once we know which direction intake is
-            if(gamepad1.left_trigger > 0){
-                intake.setPower(1);
+            if(gamepad2.left_trigger > 0){
+                intakePower = Range.clip(gamepad2.left_trigger, 0, 0.67);
+                intake.setPower(intakePower);
             }
             else{
                 intake.setPower(0);
             }
+
+            if(gamepad2.right_trigger > 0){
+                outtakePower = Range.clip(gamepad2.right_trigger, 0, 0.9);
+                outtake.setPower(outtakePower);
+            }
+            else if(gamepad2.right_bumper){
+                outtake.setPower(0.56);
+            }else{
+                outtake.setPower(0);
+            }
+
+            telemetry.addData("outtakePower", outtakePower);
+            telemetry.update();
 
 
 
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode/MonolithDetect.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode/MonolithDetect.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.testcode;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @TeleOp(name = "MonolithDetect", group = "Testing")
 
+@Disabled
 public class MonolithDetect extends LinearOpMode{
 
     private Limelight3A limelight;
