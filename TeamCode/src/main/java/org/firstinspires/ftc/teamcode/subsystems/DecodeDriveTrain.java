0e23a19e69b571cd9df7bd3b317c5e3961986b89
branefungi <benjaminzeng3@gmail.com>
2025-11-29 17:34:44 -0500

    updated for pinpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DecodeDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DecodeDriveTrain.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -10,6 +11,8 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 public class DecodeDriveTrain {
@@ -19,9 +22,10 @@ public class DecodeDriveTrain {
     private DcMotorEx bl; //Back left motor of drivetrain
     private DcMotorEx br; //Back right motor of drivetrain
     private IMU imu;
+    GoBildaPinpointDriver pinpoint;
     private double dampSpeedRatio = 1;
-    private double dampTurnRatio  = -0.6;
-    private boolean slowMode = false;
+    private double dampTurnRatio  = -1;
+    private Pose2D pose2D;
 
 
     public DecodeDriveTrain(HardwareMap hardwareMap){                 // Motor Mapping
@@ -37,54 +41,88 @@ public class DecodeDriveTrain {
         fl.setDirection(DcMotorEx.Direction.REVERSE);
         bl.setDirection(DcMotorEx.Direction.REVERSE);
 
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        imu.initialize(parameters);
-    }
+//        imu = hardwareMap.get(IMU.class, "imu");
+//        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+//                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+//                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+//        imu.initialize(parameters);
 
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        configurePinpoint();
+    }
+    public void configurePinpoint(){
+        /*
+         *  The X pod offset refers to how far sideways from the tracking point the X (forward) odometry pod is.
+         *  Left of the center is a positive number, right of center is a negative number.
+         *
+         *  The Y pod offset refers to how far forwards from the tracking point the Y (strafe) odometry pod is.
+         *  Forward of center is a positive number, backwards is a negative number.
+         */
+        pinpoint.setOffsets(9.25, -0.2, DistanceUnit.INCH); //these are tuned for 3110-0002-0001 Product Insight #1
+
+        /*
+         * Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
+         * the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
+         * If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
+         * number of ticks per unit of your odometry pod.  For example:
+         *     pinpoint.setEncoderResolution(13.26291192, DistanceUnit.MM);
+         */
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+
+        /*
+         * Set the direction that each of the two odometry pods count. The X (forward) pod should
+         * increase when you move the robot forward. And the Y (strafe) pod should increase when
+         * you move the robot to the left.
+         */
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        pinpoint.resetPosAndIMU();
+    }
 
     public void Teleop(Gamepad gamepad, Telemetry telemetry, boolean fieldCentric) {
         Teleop(gamepad, telemetry, false, fieldCentric);
     }
 
     public void Teleop(Gamepad gamepad, Telemetry telemetry, boolean showTelemetry, boolean field ){ //Code to be run in Teleop Mode void Loop at top level
+        pinpoint.update();
+        pose2D = pinpoint.getPosition();
+        double heading   = -pose2D.getHeading(AngleUnit.RADIANS);
+
+        double PowerFL;
+        double PowerFR;
+        double PowerBL;
+        double PowerBR;
+
+        double y = Range.clip(-gamepad.left_stick_y, -1, 1);
+        //left stick x value
+        double x = Range.clip(-gamepad.left_stick_x, -1, 1);
+        //right stick x value
+        double rx = Range.clip(-gamepad.right_stick_x, -1, 1);
+
         if (field){
             double max;
-            YawPitchRollAngles robotOrientation;
-            robotOrientation = imu.getRobotYawPitchRollAngles();
+//            YawPitchRollAngles robotOrientation;
+//            robotOrientation = imu.getRobotYawPitchRollAngles();
             if(gamepad.dpad_up){
-                imu.resetYaw();
+                pinpoint.resetPosAndIMU();
             }
-
-
-            if(gamepad.left_stick_button){
-                slowMode = !slowMode;
-            }
-            if(slowMode){
+            if(gamepad.right_bumper){
                 dampSpeedRatio = 0.25;
                 dampTurnRatio = -0.2;
             }else{
                 dampSpeedRatio = 1;
                 dampTurnRatio = -0.6;
             }
-            double heading   = robotOrientation.getYaw(AngleUnit.RADIANS);
 
-            // front/back
-            double axial   = -gamepad.left_stick_y * Math.cos(heading) + -gamepad.left_stick_x * Math.sin(heading);
-            // strafe
-            double lateral =  -gamepad.left_stick_y * Math.sin(heading) + gamepad.left_stick_x * Math.cos(heading);
-            // turn
-            // Now use these simple methods to extract each angle
-            // (Java type double) from the object you just created:
+            double axial   = y * Math.cos(heading) - x * Math.sin(heading);
+            double lateral = 1.1 * y * Math.sin(heading) + x * Math.cos(heading);
 
-            double yaw     =  0.8 * -gamepad.right_stick_x;
+            double turn     =  0.8 * -gamepad.right_stick_x;
 
-            double PowerFL = dampSpeedRatio*(axial + lateral) + yaw*dampTurnRatio;
-            double PowerFR = dampSpeedRatio*(axial - lateral) - yaw*dampTurnRatio;
-            double PowerBL   = dampSpeedRatio*(axial - lateral) + yaw*dampTurnRatio;
-            double PowerBR  = dampSpeedRatio*(axial + lateral) - yaw*dampTurnRatio;
+            PowerFL = dampSpeedRatio*(axial - lateral) + turn*dampTurnRatio;
+            PowerFR = dampSpeedRatio*(axial + lateral) - turn*dampTurnRatio;
+            PowerBL = dampSpeedRatio*(axial + lateral) + turn*dampTurnRatio;
+            PowerBR = dampSpeedRatio*(axial - lateral) - turn*dampTurnRatio;
 
             // Normalize the values so no wheel power exceeds 100%
             max = Math.max(Math.abs(PowerFL), Math.abs(PowerFR));
@@ -97,31 +135,13 @@ public class DecodeDriveTrain {
                 PowerBL   /= max;
                 PowerBR  /= max;
             }
+            //telemetry.addData("heading",Math.toDegrees(heading));
             fl.setPower(PowerFL);
             fr.setPower(PowerFR);
             bl.setPower(PowerBL);
             br.setPower(PowerBR);
         }
         else{
-
-            double y = Range.clip(-gamepad.left_stick_y, -1, 1);
-            //left stick x value
-            double x = Range.clip(-gamepad.left_stick_x, -1, 1);
-            //right stick x value
-            double rx = Range.clip(-gamepad.right_stick_x, -1, 1);
-
-            if(gamepad.left_stick_button){
-                slowMode = !slowMode;
-            }
-            if(slowMode){
-                dampSpeedRatio = 0.25;
-                dampTurnRatio = -0.2;
-            }else{
-                dampSpeedRatio = 1;
-                dampTurnRatio = -0.6;
-            }
-
-
             if(gamepad.right_bumper){
                 dampSpeedRatio = 0.21;
                 dampTurnRatio = -0.2; // -0.15
@@ -130,35 +150,35 @@ public class DecodeDriveTrain {
                 dampTurnRatio = -0.6;
             }
 
-            double flPower = (y - x) * dampSpeedRatio + dampTurnRatio * rx;
-            double frPower = (y + x) * dampSpeedRatio - dampTurnRatio * rx;
-            double blPower = (y + x) * dampSpeedRatio + dampTurnRatio * rx;
-            double brPower = (y - x) * dampSpeedRatio - dampTurnRatio * rx;
+            PowerFL = (y - x) * dampSpeedRatio + dampTurnRatio * rx;
+            PowerFR = (y + x) * dampSpeedRatio - dampTurnRatio * rx;
+            PowerBL = (y + x) * dampSpeedRatio + dampTurnRatio * rx;
+            PowerBR = (y - x) * dampSpeedRatio - dampTurnRatio * rx;
 
-            double maxFront = Math.max(flPower, frPower);
-            double maxBack = Math.max(blPower, brPower);
+            double maxFront = Math.max(PowerFL, PowerFR);
+            double maxBack = Math.max(PowerBL, PowerBR);
             double maxPower = Math.max(maxFront, maxBack);
 
             if (maxPower > 1.0) {
-                flPower /= maxPower;
-                frPower /= maxPower;
-                blPower /= maxPower;
-                brPower /= maxPower;
+                PowerFL /= maxPower;
+                PowerFR /= maxPower;
+                PowerBL /= maxPower;
+                PowerBR /= maxPower;
             }
             //finally moving the motors
-            fl.setPower(flPower);
-            bl.setPower(blPower);
-            fr.setPower(frPower);
-            br.setPower(brPower);
-
-            if(showTelemetry) {
-
-                telemetry.addData("FL Power", flPower);
-                telemetry.addData("BL Power", blPower);
-                telemetry.addData("FR Power", frPower);
-                telemetry.addData("BR Power", brPower);
-            }
-
+            fl.setPower(PowerFL);
+            bl.setPower(PowerBL);
+            fr.setPower(PowerFR);
+            br.setPower(PowerBR);
+        }
+        if(showTelemetry) {
+            telemetry.addData("X coordinate (IN)", pose2D.getX(DistanceUnit.INCH));
+            telemetry.addData("Y coordinate (IN)", pose2D.getY(DistanceUnit.INCH));
+            telemetry.addData("Heading angle (DEG)", pose2D.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("FL Power", PowerFL);
+            telemetry.addData("BL Power", PowerBL);
+            telemetry.addData("FR Power", PowerFR);
+            telemetry.addData("BR Power", PowerBR);
         }
 
     }
