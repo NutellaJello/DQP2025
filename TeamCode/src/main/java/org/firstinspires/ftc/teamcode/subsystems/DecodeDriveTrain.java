da5e3405332c77549595918a1558058a9d645c56
branefungi <benjaminzeng3@gmail.com>
2026-01-29 18:20:10 -0500

    Update DecodeDriveTrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DecodeDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DecodeDriveTrain.java
@@ -59,7 +59,7 @@ public class DecodeDriveTrain {
          *  The Y pod offset refers to how far forwards from the tracking point the Y (strafe) odometry pod is.
          *  Forward of center is a positive number, backwards is a negative number.
          */
-        pinpoint.setOffsets(-2, 0, DistanceUnit.INCH); //these are tuned for 3110-0002-0001 Product Insight #1
+        pinpoint.setOffsets(-2.36, -0.94, DistanceUnit.INCH); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
          * Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
@@ -75,7 +75,7 @@ public class DecodeDriveTrain {
          * increase when you move the robot forward. And the Y (strafe) pod should increase when
          * you move the robot to the left.
          */
-        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD,
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED,
                 GoBildaPinpointDriver.EncoderDirection.FORWARD);
         pinpoint.resetPosAndIMU();
     }
