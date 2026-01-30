0bb5a4f72b4973459bf401ffe7be6e43df826897
Alex Gan <92696056+NutellaJello@users.noreply.github.com>
2026-01-30 13:04:15 -0500

    Add @Disabled annotation to RedSideFar class
    Use the autos in the "Autos" folder for the new robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedSideFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedSideFar.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.DecodeDriveTrain;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
+@Disabled
 @Autonomous(name = "Red Side Far", group = "Autos")
 public class RedSideFar extends OpMode {
     private DecodeDriveTrain drivetrain;
